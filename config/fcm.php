ae3c92154afd742956220deac31f9a5ff7088922
Arturo Perez <arturo@lanzadora.com.mx>
2021-02-15 14:18:02 -0600

    changed fcm notification token

--- config/fcm.php
+++ config/fcm.php
@@ -5,8 +5,8 @@ return [
     'log_enabled' => false,
 
     'http' => [
-        'server_key' => env('FCM_SERVER_KEY', 'AAAAqahv1Cw:APA91bEd3MCqyxBFRnM9DtgTh0r95PA2NdON4gmXidUn-TYQMQlfaxO4qY7hyHBoAne4t2VkhAOJVUAveUD1IsEufhqk7xPlCu2WZJw1YszVqICk1VZwsEcHWy0ju1wjhc105NHYqxEw'),
-        'sender_id' => env('FCM_SENDER_ID', '728675374124'),
+        'server_key' => env('FCM_SERVER_KEY', 'AAAAvQBzV6g:APA91bG-KI7q81ZYqgsCedTtprcZe_1FQMYnHhSydfVexAA-Cq9GptLaIzjhclXNrQtRrqklcaWpVvLww1lUoKN6xlFoUL2pkqrDi3Vuo5hUE-JreyG4-WJdVjCGOE5gozXaE6SbpQon'),
+        'sender_id' => env('FCM_SENDER_ID', '811756378024'),
         'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
         'timeout' => 30.0, // in second
