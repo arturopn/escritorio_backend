06689f8d83457e0a3ba15453a0a70821845b72fa
Arturo Perez <arturo@lanzadora.com.mx>
2021-02-15 14:09:27 -0600

    fill notificaiton function

--- app/Http/Controllers/UsersLawyerChatAPIController.php
+++ app/Http/Controllers/UsersLawyerChatAPIController.php
@@ -157,26 +157,26 @@ class UsersLawyerChatAPIController extends AppBaseController
 
     public function sendNotification (Request $request)
     {
-        // $data = $request->all();
-        // $notificationBuilder = new PayloadNotificationBuilder();
-        // $notificationBuilder->setTitle('¡Tienes un nuevo mensaje!')
-        //         ->setBody($request->description);
+        $data = $request->all();
+        $notificationBuilder = new PayloadNotificationBuilder();
+        $notificationBuilder->setTitle('¡Tienes un nuevo mensaje!')
+                ->setBody($request->description);
 
-        // $notification = $notificationBuilder->build();
-        // // $token = "eHExYLXTKj8:APA91bH4kXrWSigXjI_UaRWUbxdgMga1OiZ5agG28Z97uc0MUoZ90EtPM3CZ_9k3wqVvquNH4Jy3svIumJYRn1gWyLhgWX85PBByACtfowzyG37yHzFa7GZFpt9PNikiKJgWCw_Zt1El";
-        // $tokens = User::whereNotNull('firebase_registration_token')->get(['firebase_registration_token']);
-        // $tokenArr = array();
-        // if(!empty($tokenArr)){
-        // foreach ($tokens as $token) {
-        //   array_push($tokenArr, $token->firebase_registration_token);
-        // }
+        $notification = $notificationBuilder->build();
+        // $token = "eHExYLXTKj8:APA91bH4kXrWSigXjI_UaRWUbxdgMga1OiZ5agG28Z97uc0MUoZ90EtPM3CZ_9k3wqVvquNH4Jy3svIumJYRn1gWyLhgWX85PBByACtfowzyG37yHzFa7GZFpt9PNikiKJgWCw_Zt1El";
+        $tokens = User::whereNotNull('firebase_registration_token')->get(['firebase_registration_token']);
+        $tokenArr = array();
+        if(!empty($tokenArr)){
+        foreach ($tokens as $token) {
+          array_push($tokenArr, $token->firebase_registration_token);
+        }
 
-        // $downstreamResponse = FCM::sendTo($tokenArr, null, $notification, null);
+        $downstreamResponse = FCM::sendTo($tokenArr, null, $notification, null);
 
-        // $downstreamResponse->numberSuccess();
-        // $downstreamResponse->numberFailure();
-        // $downstreamResponse->numberModification();
-        // }
+        $downstreamResponse->numberSuccess();
+        $downstreamResponse->numberFailure();
+        $downstreamResponse->numberModification();
+        }
         return $this->sendSuccess('Message Send');
     }
 }
